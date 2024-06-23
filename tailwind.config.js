20a7dabaeefb875e50c0a471d32ed79c39c5632b
blackdreamer15 <bakijessy@gmail.com>
2024-06-23 15:14:36 +0000

    chore: add light and dark mode to themes array

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    // darkMode: ["class", '[data-theme="dark"]'],
     theme: {
         extend: {},
     },
     plugins: [require("@tailwindcss/typography"), require("daisyui")],
-    daisyui: {},
+    daisyui: {
+        themes: ["light", "dark"],
+    },
 };
