9db80186ced896d167ad305178be541ad983e4d8
blackdreamer15 <bakijessy@gmail.com>
2024-06-23 16:20:52 +0000

    chore: register routes for login, register and home pages

--- src/configs/router.tsx
+++ src/configs/router.tsx
@@ -1,10 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import NotFound from "../pages/NotFound";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: "",
-        errorElement: "",
+        element: <Home />,
+        errorElement: <NotFound />,
+    },
+    {
+        path: "login",
+        element: <Login />,
+        errorElement: <NotFound />,
+    },
+    {
+        path: "register",
+        element: <Register />,
+        errorElement: <NotFound />,
     },
 ]);
 
