cba6f501a50f185bae46778ed53c80c36dd023f7
blackdreamer15 <bakijessy@gmail.com>
2024-07-11 21:15:56 +0000

    chore: Update router configuration and component imports

--- src/configs/router.tsx
+++ src/configs/router.tsx
@@ -1,31 +1,70 @@
-import { createBrowserRouter } from "react-router-dom";
+// import { useEffect } from "react";
+// import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ForgotPassword from "../pages/ForgotPassword";
-import NotFound from "../pages/NotFound";
+// import NotFound from "../pages/NotFound";
+import AdminDashboard from "../pages/AdminDashboard";
+// import PageTitle from "../components/PageTitle";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />,
-        errorElement: <NotFound />,
-    },
-    {
-        path: "login",
-        element: <Login />,
-        errorElement: <NotFound />,
-    },
-    {
-        path: "register",
-        element: <Register />,
-        errorElement: <NotFound />,
-    },
-    {
-        path: "forgot-password",
-        element: <ForgotPassword />,
-        errorElement: <NotFound />,
-    },
-]);
+function RouterConfig() {
+	// const { pathname } = useLocation();
 
-export default router;
+	// useEffect(() => {
+	// 	window.scrollTo(0, 0);
+	// }, [pathname]);
+
+	return (
+		<Routes>
+			<Route
+				index
+				element={
+					<>
+						{/* <PageTitle title="Home | RTMS" /> */}
+						<Home />
+					</>
+				}
+			/>
+			<Route
+				path="/auth/login"
+				element={
+					<>
+						{/* <PageTitle title="Login | TailAdmin - Tailwind CSS Admin Dashboard Template" /> */}
+						<Login />
+					</>
+				}
+			/>
+			<Route
+				path="/auth/register"
+				element={
+					<>
+						{/* <PageTitle title="Register | TailAdmin - Tailwind CSS Admin Dashboard Template" /> */}
+						<Register />
+					</>
+				}
+			/>
+			<Route
+				path="/auth/forgot-password"
+				element={
+					<>
+						{/* <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" /> */}
+						<ForgotPassword />
+					</>
+				}
+			/>
+			<Route
+				path="/admin/dashboard"
+				element={
+					<>
+						{/* <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" /> */}
+						<AdminDashboard />
+					</>
+				}
+			/>
+		</Routes>
+	);
+}
+
+export default RouterConfig;
