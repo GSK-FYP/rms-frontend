a67f681d044996b598989dbffc4f84539e330200
blackdreamer15 <bakijessy@gmail.com>
2024-07-11 21:54:10 +0000

    refactor: Update App component to use lazy loading for RouterConfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-import AppLayout from "./components/templates/AppLayout";
+import { useEffect, useState } from "react";
+
+import Loader from "./common/Loader";
+import RouterConfig from "./configs/router";
+// import MainLayout from "./components/templates/DashboardLayout";
 
 function App() {
-    return <AppLayout />;
+	const [loading, setLoading] = useState<boolean>(true);
+
+	useEffect(() => {
+		setTimeout(() => setLoading(false), 1000);
+	}, []);
+
+	return loading ? (
+		<Loader />
+	) : (
+		// <MainLayout>
+			<RouterConfig />
+		// </MainLayout>
+	);
 }
 
 export default App;
