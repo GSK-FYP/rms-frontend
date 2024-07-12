bad49d48562d9d3acb200b159f9c8ec7d1b55624
blackdreamer15 <bakijessy@gmail.com>
2024-07-12 07:30:26 +0000

    Use the sidebar ref and conditional styling on sidebarOpen on aside tag

--- src/components/molecules/UserSidebar.tsx
+++ src/components/molecules/UserSidebar.tsx
@@ -44,8 +44,10 @@ const UserSidebar: React.FC<UserSidebarProps> = ({
 
 	return (
 		<aside
-			className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-white duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0`}
-		></aside>
+			ref={sidebar}
+			className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-white duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
+				sidebarOpen ? "translate-x-0" : "-translate-x-full"
+			}`}></aside>
 	);
 };
 
