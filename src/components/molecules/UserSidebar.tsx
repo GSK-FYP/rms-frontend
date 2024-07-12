6a822cf798268dd26792c1af02732e47ae37ff85
blackdreamer15 <bakijessy@gmail.com>
2024-07-12 07:24:57 +0000

    refactor: Add trigger and sidebar refs to UserSidebar component

--- src/components/molecules/UserSidebar.tsx
+++ src/components/molecules/UserSidebar.tsx
@@ -13,6 +13,9 @@ const UserSidebar: React.FC<UserSidebarProps> = ({
 	const location = useLocation();
 	const { pathname } = location;
 
+	const trigger = useRef<any>(null);
+	const sidebar = useRef<any>(null);
+
 	return (
 		<aside
 			className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-white duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0`}
