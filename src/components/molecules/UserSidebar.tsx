2298cc2c13fdcc57c49d84f5824c4a2901972cfe
blackdreamer15 <bakijessy@gmail.com>
2024-07-12 07:19:43 +0000

    refactor: Update UserSidebar component to accept props

--- src/components/molecules/UserSidebar.tsx
+++ src/components/molecules/UserSidebar.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-const UserSidebar: React.FC = () => {
+interface UserSidebarProps {
+	sidebarOpen: boolean;
+	setSidebarOpen: (arg: boolean) => void;
+}
+
+const UserSidebar: React.FC<UserSidebarProps> = ({
+	sidebarOpen,
+	setSidebarOpen,
+}) => {
 	return (
 		<aside
 			className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-white duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0`}
