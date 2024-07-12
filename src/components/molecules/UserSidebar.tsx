9667bbc6ea66903808feeb30b466427400d33831
blackdreamer15 <bakijessy@gmail.com>
2024-07-12 07:23:20 +0000

    refactor: Update UserSidebar component to use location hook

--- src/components/molecules/UserSidebar.tsx
+++ src/components/molecules/UserSidebar.tsx
@@ -10,6 +10,9 @@ const UserSidebar: React.FC<UserSidebarProps> = ({
 	sidebarOpen,
 	setSidebarOpen,
 }) => {
+	const location = useLocation();
+	const { pathname } = location;
+
 	return (
 		<aside
 			className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-white duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0`}
