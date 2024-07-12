36730e80e1bbd9686c5d000a88be62821093297c
blackdreamer15 <bakijessy@gmail.com>
2024-07-12 07:27:14 +0000

    refactor: Improve UserSidebar component with click outside functionality

--- src/components/molecules/UserSidebar.tsx
+++ src/components/molecules/UserSidebar.tsx
@@ -16,6 +16,22 @@ const UserSidebar: React.FC<UserSidebarProps> = ({
 	const trigger = useRef<any>(null);
 	const sidebar = useRef<any>(null);
 
+	// close on click outside
+	useEffect(() => {
+		const clickHandler = ({ target }: MouseEvent) => {
+			if (!sidebar.current || !trigger.current) return;
+			if (
+				!sidebarOpen ||
+				sidebar.current.contains(target) ||
+				trigger.current.contains(target)
+			)
+				return;
+			setSidebarOpen(false);
+		};
+		document.addEventListener("click", clickHandler);
+		return () => document.removeEventListener("click", clickHandler);
+	});
+
 	return (
 		<aside
 			className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-white duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0`}
