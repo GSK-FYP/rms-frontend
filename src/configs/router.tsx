f798f10a6d432582f4b24de707fd5f7caad75c1a
blackdreamer15 <bakijessy@gmail.com>
2024-07-13 00:37:16 +0000

    feat: Add admin routes to router.tsx
    Added admin routes for map, users, businesses, properties and profiles with their respective components

--- src/configs/router.tsx
+++ src/configs/router.tsx
@@ -5,8 +5,14 @@ import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import ForgotPassword from "../pages/ForgotPassword";
-// import NotFound from "../pages/NotFound";
 import AdminDashboard from "../pages/admin/AdminDashboard";
+import AdminMap from "../pages/admin/AdminMap";
+import AdminAllBusinesses from "../pages/admin/AdminAllBusinesses";
+import AdminAllProperties from "../pages/admin/AdminAllProperties";
+import AdminAllUsers from "../pages/admin/AdminAllUsers";
+import AdminTransactions from "../pages/admin/AdminTransactions";
+import AdminProfile from "../pages/admin/AdminProfile";
+// import NotFound from "../pages/NotFound";
 // import PageTitle from "../components/PageTitle";
 
 function RouterConfig() {
@@ -18,47 +24,23 @@ function RouterConfig() {
 
 	return (
 		<Routes>
-			<Route
-				index
-				element={
-					<>
-						{/* <PageTitle title="Home | RTMS" /> */}
-						<Home />
-					</>
-				}
-			/>
-			<Route
-				path="/auth/login"
-				element={
-					<>
-						<Login />
-					</>
-				}
-			/>
-			<Route
-				path="/auth/register"
-				element={
-					<>
-						<Register />
-					</>
-				}
-			/>
-			<Route
-				path="/auth/forgot-password"
-				element={
-					<>
-						<ForgotPassword />
-					</>
-				}
-			/>
-			<Route
-				path="/admin/overview"
-				element={
-					<>
-						<AdminDashboard />
-					</>
-				}
-			/>
+			<Route path="/" element={<Home />} />
+			<Route path="/auth/login" element={<Login />} />
+			<Route path="/auth/register" element={<Register />} />
+			<Route path="/auth/forgot-password" element={<ForgotPassword />} />
+			{/* <Route path='auth/reset-password/:token' element={<ResetPassword />} />
+			<Route exact path='auth/verify/:token' element={<VerifyEmail />} /> */}
+
+			<Route path="/admin/*">
+				<Route path="overview" element={<AdminDashboard />} />
+				<Route path="map" element={<AdminMap />} />
+				<Route path="businesses" element={<AdminAllBusinesses />} />
+				<Route path="properties" element={<AdminAllProperties />} />
+				<Route path="users" element={<AdminAllUsers />} />
+				<Route path="transactions" element={<AdminTransactions />} />
+				<Route path="profile" element={<AdminProfile />} />
+				{/* <Route path="contact-support" element={} /> */}
+			</Route>
 		</Routes>
 	);
 }
