542fa03bc75724d9d70333683fb42b6b74eaf5aa
blackdreamer15 <bakijessy@gmail.com>
2024-07-14 23:29:46 +0000

    refactor: Rename AdminMap component to AdminMapView

--- src/pages/admin/AdminMapView.tsx
+++ src/pages/admin/AdminMapView.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import AdminDashboardLayout from "../../components/templates/AdminDashboardLayout";
 
-const AdminMap: React.FC = () => {
+const AdminMapView: React.FC = () => {
 	return (
 		<AdminDashboardLayout>
 			<div className="grid grid-cols-1  md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-				<h1>Admin Map Page</h1>;
+				<h1>Admin Map View Page</h1>
 			</div>
 		</AdminDashboardLayout>
 	);
 };
 
-export default AdminMap;
+export default AdminMapView;
