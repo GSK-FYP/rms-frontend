78cf530304ca4d3106600f285c17e23a307a09d6
blackdreamer15 <bakijessy@gmail.com>
2024-07-14 23:30:06 +0000

    feat: Add UserTransactions page component

---
+++ src/pages/user/UserTransactions.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserDashboardLayout from "../../components/templates/UserDashboardLayout";
+
+const UserTransactions: React.FC = () => {
+	return (
+		<UserDashboardLayout>
+			<div className="grid grid-cols-1  md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+				<h1>User Transactions Page</h1>
+			</div>
+		</UserDashboardLayout>
+	);
+};
+
+export default UserTransactions;
