77875ccfb58ab147353f42af4ff8b015e90fb3fa
Baki Jessy Justice Julien <bakijessy@gmail.com>
2024-07-16 15:40:38 +0000

    Revert "Add UIs for user and admin dashboard components and pages"

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import { useEffect, useState } from "react";
-
-import Loader from "./common/Loader";
-import RouterConfig from "./configs/router";
-// import MainLayout from "./components/templates/DashboardLayout";
-
 function App() {
-	const [loading, setLoading] = useState<boolean>(true);
 
-	useEffect(() => {
-		setTimeout(() => setLoading(false), 1000);
-	}, []);
-
-	return loading ? (
-		<Loader />
-	) : (
-		// <MainLayout>
-			<RouterConfig />
-		// </MainLayout>
-	);
+  return (
+    <div>
+      App
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./styles/style.css";
-import "./styles/satoshi.css";
-import "jsvectormap/dist/jsvectormap.css";
-import "flatpickr/dist/flatpickr.min.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
