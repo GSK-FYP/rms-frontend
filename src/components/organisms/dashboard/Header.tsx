fc673c28531d02d17e59970cdabbea670af47d06
blackdreamer15 <bakijessy@gmail.com>
2024-07-16 06:31:45 +0000

    refactor: Update structure of props of Header component

--- src/components/organisms/dashboard/Header.tsx
+++ src/components/organisms/dashboard/Header.tsx
@@ -1,13 +1,16 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import DropdownMessage from "../../Header/DropdownMessage";
 import DropdownNotification from "../../Header/DropdownNotification";
 import DropdownUser from "../../Header/DropdownUser";
 import DarkModeSwitcher from "../../molecules/DarkModeSwitcher";
 
-const Header = (props: {
+interface HeaderProps {
 	sidebarOpen: string | boolean | undefined;
 	setSidebarOpen: (arg0: boolean) => void;
-}) => {
+}
+
+const Header: React.FC<HeaderProps> = ({ sidebarOpen, setSidebarOpen }) => {
 	return (
 		<header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
 			<div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
