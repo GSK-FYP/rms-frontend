95591f99bdf0c206439cccc649e950bbaaf3b8df
blackdreamer15 <bakijessy@gmail.com>
2024-07-18 19:47:47 +0000

    feat: Add TestimonialsCard component to landing page

---
+++ src/components/atoms/landing/TestimonialsCard.tsx
@@ -0,0 +1,48 @@
+import CommaIcon from "../../../icons/CommaIcon";
+import React from "react";
+
+interface TestimonialsCardProps {
+	cardText: string;
+	name: string;
+	position: string;
+	src: string;
+}
+
+const TestimonialsCard: React.FC<TestimonialsCardProps> = ({
+	cardText,
+	name,
+	position,
+	src,
+}) => {
+	return (
+		<figure className="relative rounded-2xl bg-white p-6 shadow-xl shadow-slate-900/10">
+			<div className="absolute left-6 top-6 fill-slate-100">
+				<CommaIcon />
+			</div>
+			<div className="relative">
+				<blockquote>
+					<p className="text-lg leading-8 sm:tracking-[0.003em] lg:tracking-normal">
+						{cardText}
+					</p>
+				</blockquote>
+				<figcaption className="mt-6 flex justify-between border-t border-slate-100 pt-6">
+					<div className="flex flex-col justify-between">
+						<h3 className="mt-1 font-semibold">{name}</h3>
+						<p className="text-sm tracking-wide text-slate-500">{position}</p>
+					</div>
+					<div>
+						<img
+							src={src}
+							alt="profile img"
+							width={56}
+							height={56}
+							className="rounded-full"
+						/>
+					</div>
+				</figcaption>
+			</div>
+		</figure>
+	);
+};
+
+export default TestimonialsCard;
