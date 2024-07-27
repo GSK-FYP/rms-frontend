cbd3af974af6118b007b3be81ddba235316f89f4
blackdreamer15 <bakijessy@gmail.com>
2024-07-27 19:58:40 +0000

    chore: Implement structure of Home page

---
+++ src/pages/landing/Home.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/organisms/landing/Header";
+import MainContainer from "../../components/organisms/landing/MainContainer";
+import Footer from "../../components/molecules/landing/Footer";
+
+function Home() {
+	return (
+		<>
+			<Header />
+			<MainContainer />
+			<Footer />
+		</>
+	);
+}
+
+export default Home;
