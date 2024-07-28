13715e82ea93d2b40af89fe3fb70b8b159fa106b
blackdreamer15 <bakijessy@gmail.com>
2024-07-28 03:07:24 +0000

    feat: Add Header section to landing page

---
+++ src/components/organisms/landing/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../molecules/landing/Navbar";
+
+const Header: React.FC = () => {
+	return (
+		<header className="py-10">
+			<Navbar />
+		</header>
+	);
+};
+
+export default Header;
